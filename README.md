fd2282dd56c2ea5afdd841af2e6a255a5fad4a92
Mark B <mark.bullard42@gmail.com>
2019-06-25 14:52:34 -0400

    finished up app.js file and ready for testing/debugging. Added general use description for the application

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # Train-Scheduler
+
 Train Scheduler application that uses firebase to store data
 
 # Website
+
 https://gudbrandr42.github.io/Train-Scheduler/
 
 # Repo
+
 https://github.com/Gudbrandr42/Train-Scheduler
 
+# Firebase Assignment - Train Scheduler
+
+_Because that Rock Paper Scissors Game is Too Damn Hard_
+
+### How it works
+
+In this assignment, I created a train schedule application that incorporates Firebase to hos arrival and departure data. The app should be able to retrieve and manipulate the data using Moment.js.
+
+The use will input data, via the form fields and populate them in the table for display while simutaneously storing the information in Firebase.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,15 +13,23 @@ $(document).ready(function() {
   firebase.initializeApp(firebaseConfig);
   var database = firebase.database();
 
-//add function for trains
-  $("#addTrain").on("click", function (event) {
+  //add function for trains
+  $("#addTrain").on("click", function(event) {
     event.preventDefault();
 
     // Grabs user input
-    var trainName = $("#trainNameInput").val().trim();
-    var trainDest = $("#destinationInput").val().trim();
-    var firstTrain = $("#firstTrainInput").val().trim();
-    var trainFreq = $("#freqInput").val().trim();
+    var trainName = $("#trainNameInput")
+      .val()
+      .trim();
+    var trainDest = $("#destinationInput")
+      .val()
+      .trim();
+    var firstTrain = $("#firstTrainInput")
+      .val()
+      .trim();
+    var trainFreq = $("#freqInput")
+      .val()
+      .trim();
 
     //Creates temporary local object for holding data
     var newTrain = {
@@ -36,6 +44,62 @@ $(document).ready(function() {
 
     alert("Train successfully added!"); //tells user input saved/added
 
+    //clear textboxes
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstTrainInput").val("");
+    $("#freqInput").val("");
+  });
 
+  //this creates an even to add trains into the database and add in the html when user inputs data
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val());
 
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var firstTrain = childSnapshot.val().start;
+    var trainFreq = childSnapshot.val().frequency;
+
+    var trainFreq;
+    var firstTime = 0; //entered on the entry form.
+
+    //time conversion going back 1 year
+    var firstTimeConverted = moment(firstTime, "HH:mm").substract(1, "years");
+    console.log(firstTimeConverted);
+
+    //current time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+    //and the difference between times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERECE IN TIME: " + diffTime);
+
+    //time apart
+    var tRemainder = diffTime % trainFreq;
+    console.log(tRemainder);
+
+    //time until train
+    var tMinutesTillTrain = trainFreq - tRemainder;
+    console.log("MINUTES UNTIL TRAIN: " + tMinutesTillTrain);
+
+    //next train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+
+    //add train data into the table
+    $("#train-table, tbody").append(
+      "<tr><td>" +
+        trainName +
+        "</td><td>" +
+        trainDest +
+        "</td><td>" +
+        trainFreq +
+        "</td><td>" +
+        moment(nextTrain).format("HH:mm") +
+        "</td><td>" +
+        tMinutesTillTrain +
+        "</td></tr>"
+    );
+  });
 });
